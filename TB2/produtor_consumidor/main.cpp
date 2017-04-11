5b88dc0a94391cc6d9a15419320c3b85a69bcb81
RobertozsCunha <robertocunha@poli.ufrj.br>
2017-04-11 18:53:20 -0300

    acessando memoria compartilhada

--- TB2/produtor_consumidor/main.cpp
+++ TB2/produtor_consumidor/main.cpp
@@ -2,80 +2,87 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <ctime>
 
 
 using namespace std;
 
 #define NUM 10
-#define SIZE 10
+#define Np 2
+#define Nc 10
+#define BUFFER 100
 #define ALEATORY 100
 
-void *print(void *thread_id){
-  printf("sou a thread %d\n", thread_id);
-  pthread_exit(NULL);
-}
-
-int consumidor(int *buffer, int size){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+void *consumidor(void *buffer){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
   // PERCORRE O VETOR EM BUSCA DE UMA POSIÇÃO DIFERENTE DE 0
-  for(int i = 0; i < size; i++){
-    if(buffer[i] != 0){//VERIFICA SE O NÚMERO É PRIMO E RETORNA
-      cout << buffer[i] << endl;
-      buffer[i] = 0;
-      return 1;
-    }else{//RETORNA UM ERRO
-      return -1;
+  int *buff = (int *)buffer;
+  for(int i = 0; i < BUFFER; i++){
+    if(buff[i] != 0){//VERIFICA SE O NÚMERO É PRIMO E RETORNA
+      cout << buff[i] << endl;
+      buff[i] = 0;
+      break;
     }
   }
 }
 
-int produtor(int *buffer, int size){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+void *produtor(void *buffer){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
   // GERAR NÚMEROS ALEATÓRIOS
-  srand(time(NULL));
-  int aleatory_number = rand() % ALEATORY + 1;
+  int *buff = (int *)buffer;
 
   // PERCORRRER O ARRAY ATÉ ENCONTRAR UMA POSIÇÃO VAZIA, CASO CONTRÁRIO, AGUARDAR VAGAR UMA POSIÇÃO
-  for(int i = 0; i < size; i++){
-    if(buffer[i] == 0){//ENCONTROU UM LUGAR VAZIO
-      buffer[i] = aleatory_number;
-      return 1;
-    }else{//BUFFER CHEIO
-      return -1;
+  for(int i = 0; i < BUFFER; i++){
+    if(buff[i] == 0){//ENCONTROU UM LUGAR VAZIO
+      // srand(time(NULL));
+      int aleatory_number = rand() % ALEATORY + 1;
+      buff[i] = aleatory_number;
+      break;
     }
   }
 }
 
 void start_buffer(int *buffer){
-  for(int i = 0; i < SIZE; i++){
+  for(int i = 0; i < BUFFER; i++){
     buffer[i] = 0;
   }
 }
 
-int main(int Np, char *args[]){
+int main(int argc, char *args[]){
   cout << "Produtor Consumidor" << endl;
   // ================== INICIALIZANDO O BUFFER =================
-  int *buffer = NULL;
-  buffer = new int[SIZE];
-  start_buffer(buffer);
-  for(int i = 0; i < SIZE; i++){
+  int *buffer = NULL; //Cria um ponteiro para inteiro
+  buffer = new int[BUFFER]; //Inicializa um vetor de inteiros
+  start_buffer(buffer); // Preenhce com zeros
+  // produtor(buffer, SIZE);
+  // consumidor(buffer, SIZE);
+  // ================= Inicializa as threads ========================
+  pthread_t thread[Np];
+  // ==================== THREADS PRODUTORAS ======================
+  for(int t = 0; t < Np; t++){
+    if(pthread_create(&thread[t], NULL, produtor, (void *)buffer) != 0){
+      break;
+    }
+    // cout << "thread " << t << " criada" << endl;
+  }
+
+  for(int i = 0; i < Np; i++)
+    pthread_join(thread[i], NULL);
+
+  cout << endl;
+  for(int i = 0; i < BUFFER; i++){
+
     cout << buffer[i] << " ";
   }
-  produtor(buffer, SIZE);
-  consumidor(buffer, SIZE);
+  cout << endl;
 
-  // Inicializa as threads
-  // pthread_t thread;
-  // int erro;
-  // int id_thread, t;
-  //
-  // for(t = 0; t < NUM; t++){
-  //   printf("Criando thread %d\n", t);
-  //   if(pthread_create(&thread, NULL, print, (void *)t) != 0){
-  //     printf("Erro número %d\n", erro);
+  // ==================== THREADS CONSUMIDORAS ======================
+  // for(int t = 0; t < Nc; t++){
+  //   if(pthread_create(&thread, NULL, consumidor, (void *)(intptr_t)buffer) != 0){
   //     exit(-1);
   //   }
   // }
-  // pthread_exit(NULL);
+
+  pthread_exit(NULL);
   // ================ LIBERANDO A MEMÓRIA UTILIZADA NO BUFFER ==============
   delete [] buffer;
   buffer = NULL;
