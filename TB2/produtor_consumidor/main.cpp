12e8f2026d7eead083250c120b04b9b8aee1002d
RobertozsCunha <robertocunha@poli.ufrj.br>
2017-04-11 14:20:51 -0300

    funcao produtor/consumidor

--- TB2/produtor_consumidor/main.cpp
+++ TB2/produtor_consumidor/main.cpp
@@ -16,26 +16,39 @@ void *print(void *thread_id){
   pthread_exit(NULL);
 }
 
-void *consumidor(pthread_t *thread){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
-
+int consumidor(int *buffer, int size){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+  // PERCORRE O VETOR EM BUSCA DE UMA POSIÇÃO DIFERENTE DE 0
+  for(int i = 0; i < size; i++){
+    if(buffer[i] != 0){//VERIFICA SE O NÚMERO É PRIMO E RETORNA
+      cout << buffer[i] << endl;
+      buffer[i] = 0;
+      return 1;
+    }else{//RETORNA UM ERRO
+      return -1;
+    }
+  }
 }
 
-void *produtor(){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+int produtor(int *buffer, int size){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
   // GERAR NÚMEROS ALEATÓRIOS
   srand(time(NULL));
   int aleatory_number = rand() % ALEATORY + 1;
-  cout << aleatory_number << endl;
 
+  // PERCORRRER O ARRAY ATÉ ENCONTRAR UMA POSIÇÃO VAZIA, CASO CONTRÁRIO, AGUARDAR VAGAR UMA POSIÇÃO
+  for(int i = 0; i < size; i++){
+    if(buffer[i] == 0){//ENCONTROU UM LUGAR VAZIO
+      buffer[i] = aleatory_number;
+      return 1;
+    }else{//BUFFER CHEIO
+      return -1;
+    }
+  }
 }
 
-void inicializar_memoria(int M){
-  int *buffer = NULL;
-  buffer = new int[M];
-  for(int i = 0; i < M; i++){
+void start_buffer(int *buffer){
+  for(int i = 0; i < SIZE; i++){
     buffer[i] = 0;
   }
-  delete [] buffer;
-  buffer = NULL;
 }
 
 int main(int Np, char *args[]){
@@ -43,23 +56,26 @@ int main(int Np, char *args[]){
   // ================== INICIALIZANDO O BUFFER =================
   int *buffer = NULL;
   buffer = new int[SIZE];
+  start_buffer(buffer);
   for(int i = 0; i < SIZE; i++){
-    buffer[i] = 0;
+    cout << buffer[i] << " ";
   }
-  produtor();
-  // Inicializa as threads
-  pthread_t thread;
-  int erro;
-  int id_thread, t;
+  produtor(buffer, SIZE);
+  consumidor(buffer, SIZE);
 
-  for(t = 0; t < NUM; t++){
-    printf("Criando thread %d\n", t);
-    if(pthread_create(&thread, NULL, print, (void *)t) != 0){
-      printf("Erro número %d\n", erro);
-      exit(-1);
-    }
-  }
-  pthread_exit(NULL);
+  // Inicializa as threads
+  // pthread_t thread;
+  // int erro;
+  // int id_thread, t;
+  //
+  // for(t = 0; t < NUM; t++){
+  //   printf("Criando thread %d\n", t);
+  //   if(pthread_create(&thread, NULL, print, (void *)t) != 0){
+  //     printf("Erro número %d\n", erro);
+  //     exit(-1);
+  //   }
+  // }
+  // pthread_exit(NULL);
   // ================ LIBERANDO A MEMÓRIA UTILIZADA NO BUFFER ==============
   delete [] buffer;
   buffer = NULL;
