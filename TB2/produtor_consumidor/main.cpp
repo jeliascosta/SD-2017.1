da77d5e6293c9de429d22f3a02ba1268d0644c70
RobertozsCunha <robertocunha@poli.ufrj.br>
2017-04-11 13:50:54 -0300

    buffer e numeros aleatorios

--- TB2/produtor_consumidor/main.cpp
+++ TB2/produtor_consumidor/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <pthread.h>
 #include <stdio.h>
-#include <mutex>
-#include <condition_variable>
+#include <stdlib.h>
+#include <ctime>
+
 
 using namespace std;
 
 #define NUM 10
+#define SIZE 10
+#define ALEATORY 100
 
 void *print(void *thread_id){
   printf("sou a thread %d\n", thread_id);
@@ -17,13 +20,33 @@ void *consumidor(pthread_t *thread){//Recebe como parâmetro uma referência par
 
 }
 
-void *produtor(pthread_t *thread){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+void *produtor(){//Recebe como parâmetro uma referência para a thread e para a memória compartilhada
+  // GERAR NÚMEROS ALEATÓRIOS
+  srand(time(NULL));
+  int aleatory_number = rand() % ALEATORY + 1;
+  cout << aleatory_number << endl;
 
 }
 
-int main(int Np, char *args[]){
-  cout << "thread principal" << endl;
+void inicializar_memoria(int M){
+  int *buffer = NULL;
+  buffer = new int[M];
+  for(int i = 0; i < M; i++){
+    buffer[i] = 0;
+  }
+  delete [] buffer;
+  buffer = NULL;
+}
 
+int main(int Np, char *args[]){
+  cout << "Produtor Consumidor" << endl;
+  // ================== INICIALIZANDO O BUFFER =================
+  int *buffer = NULL;
+  buffer = new int[SIZE];
+  for(int i = 0; i < SIZE; i++){
+    buffer[i] = 0;
+  }
+  produtor();
   // Inicializa as threads
   pthread_t thread;
   int erro;
@@ -31,11 +54,14 @@ int main(int Np, char *args[]){
 
   for(t = 0; t < NUM; t++){
     printf("Criando thread %d\n", t);
-    if(erro = pthread_create(&thread, NULL, print, (void *)t)){
+    if(pthread_create(&thread, NULL, print, (void *)t) != 0){
       printf("Erro número %d\n", erro);
       exit(-1);
     }
   }
   pthread_exit(NULL);
+  // ================ LIBERANDO A MEMÓRIA UTILIZADA NO BUFFER ==============
+  delete [] buffer;
+  buffer = NULL;
   return 0;
 }
