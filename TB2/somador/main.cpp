b6bd7910c8fe4793fb3ef01e2e694276ffaeee02
RobertozsCunha <robertocunha@poli.ufrj.br>
2017-04-12 15:13:01 -0300

    test_and_set, acquire e release

--- TB2/somador/main.cpp
+++ TB2/somador/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define TRUE 1
+#define FALSE 0
 
 using namespace std;
 
+struct LOCK{
+  bool held = FALSE;
+};
+
+bool test_and_set(bool *flag){
+  bool anterior = *flag;
+  *flag = TRUE;
+  return anterior;
+}
+
+void acquire(LOCK *lock){
+  while(test_and_set(&lock->held));
+}
+
+void release(LOCK *lock){
+  lock->held = FALSE;
+}
+
 int main(int argc, char *argv[]){
+
+  /
+
   return 0;
 }
